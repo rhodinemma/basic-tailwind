27cc78faa2c8fd9f963423c3e1c0ff62d183bb8d
rhodinemma <rhodinemma10@gmail.com>
2022-10-08 17:32:13 +0300

    create table template

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/home";
 import Navbar from "./pages/Navbar";
 
 function App() {
-  return <Navbar />;
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <>
+          <Navbar />
+
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/home" exact element={<Home />} />
+            {/* <Route path="/users/:id" exact element={<Users />} />
+          <Route path="/add-user" exact element={<Add />} />
+          <Route path="/edit-user/:id" exact element={<Edit />} /> */}
+          </Routes>
+        </>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
