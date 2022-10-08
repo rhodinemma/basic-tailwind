a5098cb1d3ed749de9b1a864260304e96f85733b
rhodinemma <rhodinemma10@gmail.com>
2022-10-08 17:09:03 +0300

    add font

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -3,7 +3,9 @@ import React from "react";
 const Navbar = () => {
   return (
     <div className="w-full h-16 bg-blue-600 flex items-center px-10 py-2">
-      <h1 className="text-white text-3xl font-semibold">Rhodin</h1>
+      <h1 className="text-white text-3xl font-semibold font-sourcecodepro">
+        Basic Tailwind
+      </h1>
     </div>
   );
 };
