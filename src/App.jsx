6ffd6c3beba854d7abc610d3448f81500aec010b
rhodinemma <rhodinemma10@gmail.com>
2022-10-08 17:05:56 +0300

    create navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import "./App.css";
+import Navbar from "./pages/Navbar";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return <Navbar />;
 }
 
 export default App;
---
+++ src/pages/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="w-full h-16 bg-blue-600 flex items-center px-10 py-2">
+      <h1 className="text-white text-3xl font-semibold">Rhodin</h1>
+    </div>
+  );
+};
+
+export default Navbar;
